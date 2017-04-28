a6c9b32a982ae4cb61246fc122932d7d04565881
Dan <dgould@ksu.edu>
2017-04-28 04:09:53 -0500

    adding random neg boundary values, screw it

--- PoisonPressure.m
+++ PoisonPressure.m
@@ -1,47 +1,39 @@
-function [ P, Iterations] = PoisonPressure( ConstantMat, CenterNodes, yEnd, xEnd, UstarX, UstarY)
+function [ Pressure, Iterations] = PoisonPressure( ConstantMat, IsCenterP, P0)
 %PoisonPressure Pressure solving function
 %   Itteratively solves for the pressure field durring each timestep. Gives
 %   back the pressure field in a matrix at locations given in NodeX and
 %   NodeY. 
 
-Index=zeros(yEnd-1,xEnd-1);
-IsCenter=zeros(yEnd-1,xEnd-1);
-    for i = 1:xEnd-1 %This loop determines whether each node is central node or boundary node.
-        for j = 1:yEnd-1
-            Index(j,i) = sub2ind([yEnd-1 xEnd-1],j,i);
-            if sum(ismember(CenterNodes,Index(j,i)))==1
-                IsCenter(j,i) = true;
-            else
-                IsCenter(j,i) = false;
-            end
-        end
-    end
-    rng('default')
-P=rand(yEnd+1,xEnd+1);
-ConstantMat=rand(size(P));
-Pold=P;
+
+
+
 Iterations = 0;
 Error2 = 1;
 SOR=1; %1.7189 is optimal value.
 Beta=1;
 dx=.25;
+[ySize, xSize] = size(IsCenterP);
+Pzero=zeros(ySize+2,xSize+2);
+Pzero(2:ySize+1,2:xSize+1)=P0;
+Pold=Pzero;
+P=Pold;
 while Error2>1E-8
-    for i = (2:xEnd)
-        for j = (2:yEnd)
-            if IsCenter(j-1,i-1)==true %checks if node is central node
+    for i = (2:xSize+1)
+        for j = (2:ySize+1)
+            if IsCenterP(j-1,i-1)==true %checks if node is central node
             P(j,i) = (1-SOR).*Pold(j,i)+SOR.*(Pold(j,i+1)+P(j,i-1)+Beta^2.*(Pold(j+1,i)+P(j-1,i))+dx^2.*ConstantMat(j,i))./(2*(1+Beta^2));
             
             else %For Boundary Nodes
                 if i==2
                     P(j,i-1)=Pold(j,i);
                 end
-                if i==xEnd
+                if i==xSize+1
                     Pold(j,i+1)=Pold(j,i);
                 end
                 if j==2
                     P(j-1,i)=Pold(j,i);
                 end
-                if j==yEnd
+                if j==ySize+1
                     Pold(j+1,i)=Pold(j,i);
                 end
                     P(j,i) = (1-SOR).*Pold(j,i)+SOR.*(Pold(j,i+1)+P(j,i-1)+Beta^2.*(Pold(j+1,i)+P(j-1,i))+dx^2.*ConstantMat(j,i))./(2*(1+Beta^2));
@@ -51,7 +43,7 @@ while Error2>1E-8
         end
     end
     
-    Error2 = norm(P(2:17,2:25)-Pold(2:17,2:25)); %Calculate norm 2 error
+    Error2 = norm(P(2:ySize+1,2:xSize+1)-Pold(2:ySize+1,2:xSize+1)); %Calculate norm 2 error
     if Iterations ==5000
         h=1;
     end
@@ -59,6 +51,6 @@ while Error2>1E-8
     Iterations = Iterations+1;
 end
 
-
+Pressure=P(2:ySize+1,2:xSize+1);
 end
 
