fecf350cfb4cd24472d07d5455cc5b2e3f0ccd0d
Dan <dgould@ksu.edu>
2017-04-27 22:37:42 -0500

    Poisson Function Done. I think...

--- PoisonPressure.m
+++ PoisonPressure.m
@@ -1,4 +1,4 @@
-function [ P, Iterations] = PoisonPressure( ConstantMat, NodeX, NodeY, CenterNodes)
+function [ P, Iterations] = PoisonPressure( ConstantMat, IsCenter)
 %PoisonPressure Pressure solving function
 %   Itteratively solves for the pressure field durring each timestep. Gives
 %   back the pressure field in a matrix at locations given in NodeX and
@@ -6,19 +6,7 @@ function [ P, Iterations] = PoisonPressure( ConstantMat, NodeX, NodeY, CenterNod
 
 
 P=zeros(yEnd,xEnd);
-Index=zeros(yEnd,xEnd);
-IsCenter=zeros(yEnd,xEnd);
-    for i = 1:xEnd %This loop determines whether each node is central node or boundary node.
-        for j = 1:yEnd
-            Index(j,i) = sub2ind([yEnd xEnd],j,i);
-            if sum(ismember(CenterNodes,Index(j,i)))==1
-            IsCenter(j,i) = true;
-            else
-                IsCenter(j,i) = false;
-            end
-        end
-    end
-
+Pold=P;
 Iterations = 0;
 Error2 = 1;
 SOR=1; %1.7189 is optimal value.
@@ -27,18 +15,33 @@ while Error2>1E-8
         for j = 1:yEnd
             if IsCenter(j,i)==true %checks if node is central node
             P(j,i) = (1-SOR).*Pold(j,i)+SOR.*(Pold(j,i+1)+P(j,i-1)+Beta^2.*(Pold(j+1,i)+P(j-1,i))+dx^2.*ConstantMat)./4;
-            else
-                P(j,i) = Pold(j,i); %applies BC condition of Boundary Nodes
+            
+            else %For Boundary Nodes
+                if i==1
+                    P(j,i-1)=Pold(j,i);
+                end
+                if i==xEnd
+                    Pold(j,i+1)=Pold(j,i);
+                end
+                if j==1
+                    P(j-1,i)=Pold(j,i);
+                end
+                if j==yEnd
+                    Pold(j+1,i)=Pold(j,i);
+                end
+                
+                    P(j,i) = (1-SOR).*Pold(j,i)+SOR.*(Pold(j,i+1)+P(j,i-1)+Beta^2.*(Pold(j+1,i)+P(j-1,i))+dx^2.*ConstantMat)./4;
+                
             end
             
         end
     end
     
-    Error2 = norm((P-Pold); %Calculate norm 2 error
+    Error2 = norm(P-Pold); %Calculate norm 2 error
     Pold=P;
     Iterations = Iterations+1;
 end
 
-P=P
+
 end
 
